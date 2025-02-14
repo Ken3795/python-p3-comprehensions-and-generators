614e4d6895c72b9e99c6ef2904a119679cf86a3b
Kenneth Kipkoech <kenkipkoech57@gmail.com>
2025-02-14 17:57:16 +0300

    Complete Lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    '''returns list of even numbers from num_list'''
+    return [num for num in num_list if num % 2 == 0]
 
 def make_exclamation(sentence_list):
-    pass
+    '''returns list of sentences with an exclamation mark at the end'''
+    return [sentence + '!' for sentence in sentence_list]
